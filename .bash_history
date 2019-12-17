4a8e1eb9f80d97ea54582ca7d01013cc0f418e13
KaitoKondo <kaito.kondo@haw-hamburg.de>
2019-12-17 17:10:14 +0100

    vollständig getestet

---
+++ .bash_history
@@ -0,0 +1,87 @@
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+clear
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+clear
+PORT=tap0 make -C src/ all term
+PORT=tap0 make -C src/ all term
+clear
+ifconfig
+PORT=tap0 make -C src/ all term
+exit
+ifconfig
+g54tbvzh5zz5hh
+exit
+PORT=tap1 make -C src/ all term
+PORT=tap1 make -C src/ all term
+PORT=tap1 make -C src/ all term
+PORT=tap1 make -C src/ all term
+PORT=tap1 make -C src/ all term
+clear
+clear
+clear
+clear
+clear
+PORT=tap1 make -C src/ all term
+clear
+PORT=tap1 make -C src/ all term
+clear
+clear
+PORT=tap1 make -C src/ all term
+clear
+PORT=tap1 make -C src/ all term
+PORT=tap1 make -C src/ all term
+clear
+PORT=tap1 make -C src/ all term
+PORT=tap1 make -C src/ all term
+exit
+USER=$(id -un) ./RIOT/dist/tools/tapsetup/tapsetup -c 2
+screen -s bash
+exit
+ifconfig
+clear
+ls -l
+cd src
+make all term PORT=tap0
+clear
+make all term PORT=tap0
+exit
+make all term PORT=tap2
+cd src
+make all term PORT=tap2
+clear
+make all term PORT=tap2
+exit
+make all term PORT=tap1
+cd src
+make all term PORT=tap1
+clear
+exit
+USER=$(id -un) ./RIOT/dist/tools/tapsetup/tapsetup -c 10
+ifconfig
+screen -s bash
+exit
--- src/main.c
+++ src/main.c
@@ -39,10 +39,13 @@
 #define STATE_KLIENT 2
 
 void rescheduleInterval(void);
-void reschedThreshold(void);
-void reschedTimeout(void);
+void rescheduleThreshold(void);
+void rescheduleTimeout(void);
 bool is_addr_bigger(char *str1, char *str2);
 int16_t calculateMovingAverage(int16_t oldAverage, int16_t currentValue);
+void addClient(ipv6_addr_t* clientsList, ipv6_addr_t clientIP, int* clientsListCount);
+void clearClients(ipv6_addr_t* clientsList, int* clientsListCount);
+bool addrInList(ipv6_addr_t* clientsList, ipv6_addr_t clientIP, int* clientsListCount);
 
 static msg_t _main_msg_queue[ELECT_NODES_NUM];
 static kernel_pid_t this_main_pid;
@@ -113,9 +116,13 @@ int main(void)
 {
     bool otherIPIsHigher = false;
     bool firstRound = true;
+    bool leaderAlive = true;
     int msgCounter = 0;
     int state = 0;
     ipv6_addr_t highestAddr = {{0}};
+    ipv6_addr_t clientsList[8] = {{{0}}};
+    int clientsListCount = 0;
+    int16_t mittelwert = 0;
 
     /* this should be first */
     if (setup() != 0)
@@ -142,25 +149,81 @@ int main(void)
         case ELECT_INTERVAL_EVENT:
             LOG_DEBUG("+ interval event.\n");
             printf("%i", state);
-            if (!otherIPIsHigher)
-            {
-                if (broadcast_id(&thisAddr) < 0)
-                {
-                    printf("%s: failed\n", __func__);
-                }
-            }
-            msgCounter = 0;
             if (state == STATE_ENTDECKUNG)
             {
+                if(!otherIPIsHigher)
+                {
+                    if (broadcast_id(&thisAddr) < 0)
+                    {
+                        printf("%s: failed\n", __func__);
+                    }
+                }
+                rescheduleInterval();
+                clearClients(clientsList, &clientsListCount);
+            }else if (state == STATE_KOORDINATOR)
+            {
+                if (broadcast_sensor(mittelwert) < 0) {
+                    printf("%s: failed\n", __func__);
+                }
+                mittelwert = sensor_read();
+                puts("works as Koordinator");
+                for (int i = 0; i < clientsListCount; i++){
+                    coap_get_sensor(clientsList[i]);
+                }
                 rescheduleInterval();
             }
+            msgCounter = 0;
             break;
+
         case ELECT_BROADCAST_EVENT:
             LOG_DEBUG("+ broadcast event, from [%s]\n", (char *)m.content.ptr);
             if (is_addr_bigger(thisAddrStr, (char *)m.content.ptr))
             {
-                puts("Es liegt nicht an dir, es liegt an mir, aber ich denke nicht das es zwischen uns klppt, ich habe jemand höheren gefunden.");
-                otherIPIsHigher = true;
+                if(state == STATE_KOORDINATOR)
+                {
+                    puts("ICH WURDE GESTÜRZT");
+                    /* send initial `TICK` to start eventloop */
+                    msg_send(&interval_event.msg, this_main_pid);
+                    /* send initial `TICK` to start eventloop */
+                    msg_send(&leader_threshold_event.msg, this_main_pid);
+                    clearClients(clientsList, &clientsListCount);
+                    otherIPIsHigher = false;
+                    firstRound = true;
+                    leaderAlive = true;
+                    msgCounter = 0;
+                    state = STATE_ENTDECKUNG;
+                    memset(&highestAddr, 0, sizeof(ipv6_addr_t));
+                    mittelwert = 0;
+                }
+                else if(state == STATE_ENTDECKUNG)
+                {
+                    puts("höhere IP gefunden.");
+                    otherIPIsHigher = true;
+                }
+                else if(state == STATE_KLIENT)
+                {
+                    puts("DER KÖNIG WURDE GESTÜRZT");
+                    /* send initial `TICK` to start eventloop */
+                    msg_send(&interval_event.msg, this_main_pid);
+                    /* send initial `TICK` to start eventloop */
+                    msg_send(&leader_threshold_event.msg, this_main_pid);
+                    clearClients(clientsList, &clientsListCount);
+                    otherIPIsHigher = false;
+                    firstRound = true;
+                    leaderAlive = true;
+                    msgCounter = 0;
+                    state = STATE_ENTDECKUNG;
+                    memset(&highestAddr, 0, sizeof(ipv6_addr_t));
+                    mittelwert = 0;
+                }
+            }
+            else
+            {
+                //Broadcast my IP once, so the other node hears me
+                if (broadcast_id(&thisAddr) < 0)
+                {
+                    printf("%s: failed\n", __func__);
+                }
             }
             char highestAddrStr[40];
             ipv6_addr_to_str(highestAddrStr, &highestAddr, 40);
@@ -171,61 +234,81 @@ int main(void)
             }
             msgCounter++;
             break;
+
         case ELECT_LEADER_ALIVE_EVENT:
             LOG_DEBUG("+ leader event.\n");
-            /**
-             * @todo implement
-             */
+            leaderAlive = true;
             break;
+
         case ELECT_LEADER_TIMEOUT_EVENT:
             LOG_DEBUG("+ leader timeout event.\n");
-            /**
-             * @todo implement
-             */
+            if(leaderAlive){
+                leaderAlive = false;
+                rescheduleTimeout();
+            } else {
+                /* send initial `TICK` to start eventloop */
+                msg_send(&interval_event.msg, this_main_pid);
+                /* send initial `TICK` to start eventloop */
+                msg_send(&leader_threshold_event.msg, this_main_pid);
+                clearClients(clientsList, &clientsListCount);
+                otherIPIsHigher = false;
+                firstRound = true;
+                leaderAlive = true;
+                msgCounter = 0;
+                state = STATE_ENTDECKUNG;
+                memset(&highestAddr, 0, sizeof(ipv6_addr_t));
+                mittelwert = 0;
+            }            
             break;
+
         case ELECT_NODES_EVENT:
             LOG_DEBUG("+ nodes event, from [%s].\n", (char *)m.content.ptr);
-            /**
-             * @todo implement
-             */
+            puts("REGISTERING CLIENT!!\n");
+            ipv6_addr_t clientIP;
+            ipv6_addr_from_str(&clientIP, (char*) m.content.ptr);
+            addClient(clientsList, clientIP, &clientsListCount);
+            printf("I now have %i clients\n", clientsListCount);
             break;
+
         case ELECT_SENSOR_EVENT:
             LOG_DEBUG("+ sensor event, value=%s\n", (char *)m.content.ptr);
-            /**
-             * @todo implement
-             */
+            int16_t value = (int16_t)strtol((char *)m.content.ptr, NULL, 10);
+            mittelwert = calculateMovingAverage(mittelwert, value);
             break;
+
         case ELECT_LEADER_THRESHOLD_EVENT:
             LOG_DEBUG("+ leader threshold event.\n");
             if (firstRound)
             {
-                reschedThreshold();
+                rescheduleThreshold();
                 firstRound = false;
                 break;
             }
             printf("msgCounter ist %i\n", msgCounter);
-            if (otherIPIsHigher)
-            {
-                puts("Inf");
-            }
-            else
-            {
-                puts("Sup");
-            }
             if (otherIPIsHigher && msgCounter < 2)
             {
                 puts("Ich bin Client");
                 state = STATE_KLIENT;
+
+                char highestAddrStr[40];
+                ipv6_addr_to_str(highestAddrStr, &highestAddr, 40);
+                char thisAddrStr[40];
+                ipv6_addr_to_str(thisAddrStr, &thisAddr, 40);
+
                 if (coap_put_node(highestAddr, thisAddr) == 0)
                 {
+                    printf("Klient: %s, an Koordinator: %s",thisAddrStr, highestAddrStr);
                     printf("Success\n");
                 }
+
+                msg_send(&leader_timeout_event.msg, this_main_pid);
                 break;
             }
             else if (!otherIPIsHigher && msgCounter < 2)
             {
                 puts("Ich bin Coordinator");
                 state = STATE_KOORDINATOR;
+                rescheduleInterval();
                 break;
             }
             else
@@ -233,8 +316,11 @@ int main(void)
                 puts("Ich bin unbestimmt");
             }
 
-            reschedThreshold();
+            msgCounter = 0;
+
+            rescheduleThreshold();
             break;
+
         default:
             LOG_WARNING("??? invalid event (%x) ???\n", m.type);
             break;
@@ -261,7 +347,7 @@ void rescheduleInterval(void)
     evtimer_add_msg(&evtimer, &interval_event, this_main_pid);
 }
 
-void reschedThreshold(void)
+void rescheduleThreshold(void)
 {
     // remove existing event
     evtimer_del(&evtimer, &leader_threshold_event.event);
@@ -271,7 +357,7 @@ void reschedThreshold(void)
     evtimer_add_msg(&evtimer, &leader_threshold_event, this_main_pid);
 }
 
-void reschedTimeout(void)
+void rescheduleTimeout(void)
 {
     // remove existing event
     evtimer_del(&evtimer, &leader_timeout_event.event);
@@ -309,4 +395,35 @@ int16_t calculateMovingAverage(int16_t oldAverage, int16_t currentValue)
     int16_t result = (int16_t)((double)((((16.0 - 1.0) / 16.0) * (double)Xi) + ((1.0 / 16.0) * (double)x)));
 
     return result;
+}
+
+void addClient(ipv6_addr_t* clientsList, ipv6_addr_t clientIP, int* clientsListCount){
+    if(!addrInList(clientsList, clientIP, clientsListCount)){
+        clientsList[*clientsListCount] = clientIP;
+        *clientsListCount = *clientsListCount + 1;
+        puts("Stored client IP");
+        if(*clientsListCount < 0)
+        {
+            puts("This will never happen");
+        }
+    }
+    else
+    {
+        puts("Kannte ihn bereits");
+    }
+}
+
+void clearClients(ipv6_addr_t* clientsList, int* clientsListCount){
+        memset(clientsList, 0, sizeof(ipv6_addr_t) * 8);
+        *clientsListCount = 0;
+}
+
+bool addrInList(ipv6_addr_t* clientsList, ipv6_addr_t clientIP, int* clientsListCount){
+    for(int i = 0; i < *clientsListCount; i++)
+    {
+        if(ipv6_addr_cmp(clientsList + i, &clientIP) == 0){
+            return true;
+        }
+    }
+    return false;
 }
